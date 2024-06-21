1363a345d8f46cf14735dd193af81b511035b68d
Shibraj <shibrajdeb456@gmail.com>
2024-06-21 15:33:58 +0530

    mongo setup done

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./utils/db');
 
 const app = express();
 const port = 8000;
+
+//Connect Database
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
